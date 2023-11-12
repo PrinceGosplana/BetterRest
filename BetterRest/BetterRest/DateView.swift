1e0bdc1904973dab1ad1de0b98503048cf1373ad
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-12 07:15:45 +0200

    do not show date in the past

--- BetterRest/BetterRest/DateView.swift
+++ BetterRest/BetterRest/DateView.swift
@@ -11,8 +11,13 @@ struct DateView: View {
     @State private var wakeUp = Date.now
     
     var body: some View {
-        DatePicker("Please enter a date", selection: $wakeUp)
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
             .labelsHidden()
+    }
+    
+    func exampleDates() {
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = Date.now...tomorrow
     }
 }
 
