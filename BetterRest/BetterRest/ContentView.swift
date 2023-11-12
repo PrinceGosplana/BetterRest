0b654f376ece294f3f7a030ce0012d940d173787
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-12 06:59:45 +0200

    move stepper into separate container

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -7,15 +7,23 @@
 
 import SwiftUI
 
+struct StepperView: View {
+    @Binding var value: Double
+    
+    var body: some View {
+        Stepper("\(value.formatted()) hours",
+                value: $value,
+                in: 4...12,
+                step: 0.25)
+    }
+}
+
 struct ContentView: View {
     
     @State private var sleepAmount = 7.0
     
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", 
-                value: $sleepAmount,
-                in: 4...12,
-                step: 0.25)
+        StepperView(value: $sleepAmount)
     }
 }
 
