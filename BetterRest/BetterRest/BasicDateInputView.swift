cf08b1bdec886df7ff28c95ccd3ff444b1d0f63f
OLEKSANDR ISAIEV <nevkluchat@gmail.com>
2023-11-15 08:42:05 +0200

    add date input view

---
+++ BetterRest/BetterRest/BasicDateInputView.swift
@@ -0,0 +1,29 @@
+//
+//  BasicDateInputView.swift
+//  BetterRest
+//
+//  Created by OLEKSANDR ISAIEV on 15.11.2023.
+//
+
+import SwiftUI
+
+struct BasicDateInputView: View {
+    @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+            }
+        }
+    }
+}
+
+#Preview {
+    BasicDateInputView()
+}
